ab9e09469d90424958ab323b2d7a3972e6054d17
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-10-03 09:21:58 -0700

    Agregar lógica para verificar la mayoría de edad y mejorar el formulario de entrada en mayor_edad_web.php

--- mayor_edad_web.php
+++ mayor_edad_web.php
@@ -1,3 +1,12 @@
+<?php
+if (isset($_GET['edad'])) {
+    if($_GET['edad']>=18) {
+        $es_mayor = true;
+    } else {
+        $es_mayor = false;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +17,22 @@
 <body>
     <h1>Identificación de mayoría de edad</h1>
     <!-- form>label{¿Cuál es tu edad?}+input:number+button{Verificar la edad} -->
-    <form action="#">
-        <label for="edad">¿Cuál es tu edad?</label>
-        <input type="number" name="edad" id="edad">
-        <button>Verificar la edad</button>
+    <form method="get">
+        <label for="edad">
+            ¿Cuál es tu edad?
+        </label>
+        <input type="number"
+               name="edad"
+               id="edad"
+               max="100"
+               min="1"
+               required>
+        <button type="submit">
+            Verificar la edad
+        </button>
     </form>
+    <?php if (isset($es_mayor)) { ?>
+    <h3>Aqui vamos, ya hace hambre</h3>
+    <?php } ?>
 </body>
 </html>
